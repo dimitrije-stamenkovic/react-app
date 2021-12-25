564e9722fca57a01ea2a444a69d840de2fadb76e
dimitrije-stamenkovic <muskitos.97@gmail.com>
2021-12-25 16:56:54 +0100

    Initall commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-slate-200">
+
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center items-center h-screen">
+        {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+        <div className="max-w-3xl mx-auto ">
+
+          <p data-testid="counter" className='my-8 text-2xl text-center'>{count}</p>
+
+          <div class="flex">
+
+            <button
+              type="button"
+              data-testid="button-1"
+              onClick={() => setCount(count + 1)}
+              className="mx-1 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              +1
+            </button>
+
+            <button
+              type="button"
+              data-testid="button-2"
+              onClick={() => setCount(count + 2)}
+              className="mx-1 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              +2
+            </button>
+
+            <button
+              type="button"
+              data-testid="button-3"
+              onClick={() => setCount(count - 1)}
+              className="mx-1 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              -1
+            </button>
+
+            <button
+              type="button"
+              data-testid="button-4"
+              onClick={() => setCount(count - 2)}
+              className="mx-1 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              -2
+            </button>
+
+          </div>
+
+
+        </div>
+
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import "@testing-library/jest-dom/extend-expect";
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(cleanup)
+
+it('should take a snapshot', () => {
+    const { asFragment } = render(<App />)
+
+    expect(asFragment(<App />)).toMatchSnapshot()
+
 });
+
+
+it('incerment 1', () => {
+    const { getByTestId } = render(<App />);
+
+    fireEvent.click(getByTestId('button-1'))
+
+    expect(getByTestId('counter')).toHaveTextContent('1')
+});
+
+it('incerment 2', () => {
+    const { getByTestId } = render(<App />);
+
+    fireEvent.click(getByTestId('button-2'))
+
+    expect(getByTestId('counter')).toHaveTextContent('2')
+});
+
+it('decrement 1', () => {
+    const { getByTestId } = render(<App />);
+
+    fireEvent.click(getByTestId('button-3'))
+
+    expect(getByTestId('counter')).toHaveTextContent('-1')
+});
+
+it('decrement 2', () => {
+    const { getByTestId } = render(<App />);
+
+    fireEvent.click(getByTestId('button-4'))
+
+    expect(getByTestId('counter')).toHaveTextContent('-2')
+});
+
